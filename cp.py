0faed41ddab0cd2c4e219af0ba624b58f839c4a5
sr <srronak11@gmail.com>
2022-12-17 12:34:36 +0530

    v4

--- cp.py
+++ cp.py
@@ -1,17 +1,17 @@
 try:
     def cp():
         global b
-        with open(r"/home/ec2-user/f1.txt","w") as i:
+        with open(r"/home/ubuntu/f1.txt","w") as i:
             i.write("hello this is my jenkins pipeline")
-        with open(r"/home/ec2-user/f1.txt","r") as i:
+        with open(r"/home/ubuntu/f1.txt","r") as i:
             b=i.read()
-        with open(r"/home/ec2-user/file2.txt","x") as i:
+        with open(r"/home/ubuntu/file2.txt","x") as i:
             i.write(b)
            # print("file data has been copy successfully by create mode !!!")
     
     ## calling the cp function
     cp()
 except FileExistsError as i:
-    with open(r"/home/ec2-user/file2.txt","a") as i:
+    with open(r"/home/ubuntu/file2.txt","a") as i:
         i.write(b)
         # print("File data has been copy successfully by append mode")
